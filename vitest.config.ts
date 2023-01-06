70819e594002499ee8e6dbd0b09a743650ac171c
weiyafei <2553241022@qq.com>
2023-01-06 15:24:05 +0800

    feat: add vitest

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+/*
+ * @Description: 
+ * @Author: wsy
+ * @Date: 2023-01-06 15:04:35
+ * @LastEditTime: 2023-01-06 15:19:44
+ * @LastEditors: wsy
+ */
+import { defineConfig } from 'vitest/config'
+export default defineConfig({
+  test: {
+    globals: true,
+  },
+});
